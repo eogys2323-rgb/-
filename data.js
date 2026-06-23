29ce7753022645c88c38c26a845c331b49f7dbc0
eogys2323-rgb <eogys2323@gmail.com>
2026-06-23 17:32:47 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,549 @@
+const data = {
+
+"스티로폼":{
+images:[
+
+{
+file:"images/sf1.jpg",
+title:"스티로폼박스"
+},
+
+{
+file:"images/sf2.jpg",
+title:"완충재"
+},
+
+{
+file:"images/sf3.jpg",
+title:"재활용 마크가 있는 스티로폼"
+}
+
+],
+desc:"재활용 가능 스티로폼",
+method:"1.이물질 제거 후 분리배출",
+keywords:[
+"스티로폼",
+"포장재",
+"완충재"
+]
+},
+
+"건설용 스티로폼":{
+images:[
+
+{
+file:"images/ci1.jpg",
+title:"판넬스티로폼"
+},
+
+{
+file:"images/ci2.jpg",
+title:"판넬스티로폼"
+},
+
+{
+file:"images/ci3.jpg",
+title:"단열재 스티로폼"
+},
+
+{
+file:"images/ci4.jpg",
+title:"단열재 스티로폼"
+},
+
+{
+file:"images/ci5.jpg",
+title:"단열재 스티로폼"
+},
+
+],
+desc:"재활용 불가능 스티로폼",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"스티로폼",
+"포장재",
+"완충재"
+]
+},
+
+"유색 스티로폼":{
+images:[
+
+{
+file:"images/dt1.jpg",
+title:"유색 스티로폼"
+},
+
+{
+file:"images/dt2.jpg",
+title:"유색 스티로폼"
+},
+
+],
+desc:"재활용 불가능 스티로폼",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"스티로폼",
+"포장재",
+"완충재"
+]
+},
+
+"고무통":{
+images:[
+
+{
+file:"images/gm1.jpg",
+title:"고무통"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"고무통",
+"농업용",
+"약통"
+]
+},
+
+"점적호스":{
+images:[
+
+{
+file:"images/jh1.jpg",
+title:"점적호스"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"호스",
+"농업용",
+"점적"
+]
+},
+
+"모판":{
+images:[
+
+{
+file:"images/mp1.jpg",
+title:"모판"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"모판"
+]
+},
+
+"농약분무기":{
+images:[
+
+{
+file:"images/nb1.jpg",
+title:"농약분무기(자동)"
+},
+
+{
+file:"images/nb2.jpg",
+title:"농약분무기(수동)"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"분무기",
+"농업용",
+"농약"
+]
+},
+
+"농수관":{
+images:[
+
+{
+file:"images/ns1.jpg",
+title:"농수관"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"농수관",
+"농업용"
+]
+},
+
+"톤마대":{
+images:[
+
+{
+file:"images/tm1.jpg",
+title:"톤마대"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"마대",
+"자루",
+"톤자루"
+]
+},
+
+"트레이포트":{
+images:[
+
+{
+file:"images/tp1.jpg",
+title:"트페이포트"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"포트",
+"모종포트"
+]
+},
+
+"보온덮개":{
+images:[
+
+{
+file:"images/bd1.jpg",
+title:"보온덮개"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"덮개",
+"깔개",
+"바닥"
+]
+},
+
+"차광막":{
+images:[
+
+{
+file:"images/mak1.jpg",
+title:"차광막"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"그늘",
+"햇빛",
+"차강막",
+"검은"
+]
+},
+
+"분수호스":{
+images:[
+
+{
+file:"images/bh1.jpg",
+title:"분수호스"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"호스",
+"분수"
+]
+},
+
+"농산물망":{
+images:[
+
+{
+file:"images/nm1.jpg",
+title:"농산물망"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"망",
+"마늘",
+"양파"
+]
+},
+
+"노끈":{
+images:[
+
+{
+file:"images/nG1.jpg",
+title:"노끈"
+},
+
+],
+desc:"비수거 기타영농폐기물",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"영농폐기물",
+"끈",
+"노끈"
+]
+},
+
+"재활용 가능 비닐류":{
+images:[
+
+{
+file:"images/vn1.jpg",
+title:"과자봉지"
+},
+
+{
+file:"images/vn2.jpg",
+title:"라면봉지"
+},
+
+{
+file:"images/vn3.jpg",
+title:"재활용마크가 있는 비닐류"
+},
+
+],
+desc:"재활용 가능 비닐류",
+method:"이물질 제거 후 투명한 봉투에 담아 분리배출",
+keywords:[
+"과자",
+"커피",
+"라면",
+"비닐",
+"재활용"
+]
+},
+
+"재활용 불가능 비닐류":{
+images:[
+
+{
+file:"images/bv1.jpg",
+title:"스트래치 필름"
+},
+
+{
+file:"images/bv2.jpg",
+title:"이물질 묻은 비닐"
+},
+
+{
+file:"images/bv3.jpg",
+title:"비닐끈"
+},
+
+],
+desc:"재활용 불가능 비닐류",
+method:"종량제봉투 또는 특수종량제마대에 담아서 배출",
+keywords:[
+"과자",
+"커피",
+"라면",
+"비닐",
+"재활용"
+]
+},
+
+"유리병류":{
+images:[
+
+{
+file:"images/yr1.jpg",
+title:"음료수병,소주병,맥주병 등등"
+},
+
+{
+file:"images/yr2.jpg",
+title:"재활용마크가 있는 유리병류"
+},
+
+],
+desc:"재활용 가능 유리병류",
+method:"이물질 제거 후 투명한 봉투에 담아 분리배출",
+keywords:[
+"유리병",
+"재활용",
+"음료"
+]
+},
+
+"재활용 가능 플라스틱류":{
+images:[
+
+{
+file:"images/pr1.jpg",
+title:"물병,음료수병 등등"
+},
+
+{
+file:"images/pr2.jpg",
+title:"양념통"
+},
+
+{
+file:"images/pr3.jpg",
+title:"재활용마크가 있는 플라스틱류"
+},
+
+],
+desc:"재활용 가능 플라스틱류",
+method:"이물질 제거 후 투명한 봉투에 담아 분리배출",
+keywords:[
+"플라스틱",
+"재활용",
+"용기",
+"햇반",
+"컵라면",
+"된장",
+"고추장",
+"음료수",
+"물병"
+]
+},
+
+"형광등":{
+images:[
+
+{
+file:"images/jg1.jpg",
+title:"전구형"
+},
+
+{
+file:"images/jg2.jpg",
+title:"전구형"
+},
+
+{
+file:"images/jg3.jpg",
+title:"직관형"
+},
+
+{
+file:"images/jg4.jpg",
+title:"직관형"
+},
+
+{
+file:"images/jg5.jpg",
+title:"원형"
+},
+
+],
+desc:"형광등",
+method:"1.수거함에 배출(수거함 위치 : 면사무소 뒷편 또는 마을 전용수거함)",
+keywords:[
+"형광등",
+"전구",
+"LED",
+"등",
+"불빛"
+]
+},
+
+"LED등":{
+images:[
+
+{
+file:"images/led1.jpg",
+title:"LED 직관형"
+},
+
+{
+file:"images/led2.jpg",
+title:"LED 전구형"
+},
+
+{
+file:"images/led3.jpg",
+title:"LED 십자형"
+},
+
+{
+file:"images/led4.jpg",
+title:"LED등"
+},
+
+{
+file:"images/led5.jpg",
+title:"태양광 LED"
+},
+
+{
+file:"images/led6.jpg",
+title:"태양광 LED"
+},
+
+],
+desc:"LED등",
+method:"종량제봉투 또는 종량제특수마대에 담아서 배출",
+keywords:[
+"형광등",
+"전구",
+"LED",
+"등",
+"불빛",
+"태양광",
+]
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
